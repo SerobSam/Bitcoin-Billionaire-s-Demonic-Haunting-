aef2b6661b069337b1722b9fd5844d622cbb42b5
SerobSam <serobc40@yahoo.com>
2026-08-01 23:43:02 +0000

    commit all changes to "main"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         eachPlugin {
             when (requested.id.id) {
                 "com.android.application" -> useModule("com.android.tools.build:gradle:9.3.0")
-                "org.jetbrains.kotlin.android", "org.jetbrains.kotlin.jvm", "org.jetbrains.kotlin.plugin.serialization" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+                "org.jetbrains.kotlin.android", "org.jetbrains.kotlin.jvm", "org.jetbrains.kotlin.plugin.serialization", "org.jetbrains.kotlin.kapt" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
                 "com.google.devtools.ksp" -> useModule("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.14")
             }
 
