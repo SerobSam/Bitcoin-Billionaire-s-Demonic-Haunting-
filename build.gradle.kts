f6215496ef8421f6b6f0efc7a66c073ea41e7bfc
SerobSam <serobc40@yahoo.com>
2026-08-01 23:37:29 +0000

    commit all changes to "main"

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Root build file with common configuration for the Android application project.
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+}
